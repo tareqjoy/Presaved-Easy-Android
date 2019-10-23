c768aa93b60f76f89c863c50fdac1d75f81d0d64
Tareq Rahman Joy <31368288+tareqjoy@users.noreply.github.com>
2019-10-23 19:39:52 +0600

    Create Generalized Request Permission.java

---
+++ Generalized Request Permission.java
@@ -0,0 +1,48 @@
+    
+    
+    if(isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE, REQ_IMG_READ)){
+      //code
+    }
+    
+    
+    
+    public boolean isPermissionGranted(String permission, final int REQ_CODE) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (getActivity().checkSelfPermission(permission)
+                    == PackageManager.PERMISSION_GRANTED) {
+                // Log.v(TAG,"Permission is granted1");
+                return true;
+            } else {
+
+                //  Log.v(TAG,"Permission is revoked1");
+                ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, REQ_CODE);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            // Log.v(TAG,"Permission is granted1");
+            return true;
+        }
+    }
+    
+    
+    
+    
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_IMG_READ:
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //yay! granted
+
+                } else {
+                    //     not granted, oops
+                }
+                break;
+
+
+        }
+    }
+    
